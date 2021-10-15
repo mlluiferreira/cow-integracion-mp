3d1532d985f68cd7eac8ee70783fc972b09a8ac6
Luiz Ferreira <luiferreira@br0c02dx6jamd6n.ml.com>
2021-10-15 15:24:35 -0300

    change parser policy

--- src/main/java/org/example/learnspark/util/Parser.java
+++ src/main/java/org/example/learnspark/util/Parser.java
@@ -1,10 +1,12 @@
 package org.example.learnspark.util;
 
+import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class Parser {
 
-    private static final Gson gson = new Gson();
+    private static final Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create();
 
     private Parser() { }
 
